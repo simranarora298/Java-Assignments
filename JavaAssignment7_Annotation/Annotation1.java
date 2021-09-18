00e1e3b2e459f2ca664f45245b3eb0c909da57a6
simranarora298 <47690480+simranarora298@users.noreply.github.com>
2021-09-18 13:18:42 +0530

    Add files via upload

---
+++ JavaAssignment7_Annotation/Annotation1.java
@@ -0,0 +1,16 @@
+package capg.co;
+
+import java.lang.annotation.*;
+
+
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.METHOD)
+@interface Test{}
+
+public class Annotation1 
+{
+	public static void main(String[] args) 
+	{
+		
+	}
+}
---
+++ JavaAssignment7_Annotation/Annotation2.java
@@ -0,0 +1,28 @@
+package capg.co;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.CLASS)
+@Target({
+		ElementType.TYPE,
+		ElementType.METHOD,
+		ElementType.FIELD
+})
+@interface Info{
+		String authorID();
+		String author() default "";
+		String supervisor() default "";
+		String date();
+		String time();
+		int version();
+		String description() default "";
+}
+
+public class Annotation2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
